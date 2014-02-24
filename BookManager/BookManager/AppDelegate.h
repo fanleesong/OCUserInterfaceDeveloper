d5f33191b96d7a5c052a1776e1db1b304a5b211a
leesongfan <781957672@qq.com>
2014-02-24 16:25:13 +0800

    Initial Commit

---
+++ BookManager/BookManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BookManager
+//
+//  Created by 范林松 on 14-2-24.
+//  Copyright (c) 2014年 范林松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
